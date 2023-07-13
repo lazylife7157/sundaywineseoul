88bff2e33b656a812ff24a6039736d57f1cdb549
lazylife7157 <lazylife7157@gmail.com>
2023-07-13 19:20:41 +0900

    BnW

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,14 +47,14 @@
 		<div class="left">Sunday Wine Seoul © 2023 - All Rights Reserved</div>
 		<div class="right">
 			<a href="https://www.instagram.com/sundaywineseoul/" target="_blank">
-				<svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 448 512"
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
 					><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 						d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
 					/></svg
 				>
 			</a>
 			<a href="mailto:sundaywineseoul@gmail.com">
-				<svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 512 512"
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
 					><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 						d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
 					/></svg
@@ -65,6 +65,10 @@
 </div>
 
 <style>
+	a {
+		color: inherit;
+	}
+
 	div.container {
 		display: flex;
 		flex-direction: column;
@@ -74,16 +78,17 @@
 	}
 
 	header {
-		border-bottom: 1px solid black;
+		color: white;
+		background-color: #212529;
 	}
 
 	header > a.title {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		color: black;
 		text-decoration: none;
 		margin-bottom: 4rem;
+		color: var(--color);
 	}
 	header > a.title > h1 {
 		font-family: 'Megrim', cursive;
@@ -124,7 +129,7 @@
 		height: 0;
 		padding: 0 1rem;
 		padding-bottom: 0.5rem;
-		background-color: white;
+		background-color: #212529;
 		overflow: hidden;
 		transition: height 0.5s;
 		font-size: 0.8em;
@@ -135,12 +140,10 @@
 	}
 
 	header > nav > ul > li a {
-		color: black;
 		text-decoration: none;
 	}
 
 	header > nav > ul > li a:hover {
-		color: black;
 		text-decoration: underline;
 	}
 
@@ -156,26 +159,30 @@
 	}
 	main > div.order > a {
 		font-family: 'Abel', sans-serif;
-		color: white;
 		text-decoration: none;
 		font-size: 2rem;
-		background-color: black;
-		border-radius: 1rem;
+		color: white;
+		background-color: #212529;
 		padding: 1rem 2rem;
 	}
 	main > div.order > a:hover {
-		color: pink;
+		color: #dee2e6;
 	}
 
 	footer {
 		display: flex;
 		justify-content: space-between;
-		font-size: 1.2rem;
+		align-items: center;
 		padding: 1rem 2rem;
-		border-top: 1px solid black;
+		color: white;
+		background-color: #212529;
 	}
 	footer a {
 		padding: 0.5rem;
+	}
+	footer svg {
+		fill: white;
+		height: 1.2rem;
 	}
 
 	@media (max-width: 1024px) {
