b5ef73035d7574ce343328ac3e1d0b2ac2761f33
lazylife7157 <lazylife7157@gmail.com>
2023-09-02 14:23:04 +0900

    Fix...?

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -16,7 +16,10 @@
     <h1>
       <a href="/">sunday wine seoul</a>
     </h1>
-    <button on:click={() => (isCollapsed = !isCollapsed)} on:touchend={() => (isCollapsed = !isCollapsed)}>
+    <button
+      on:mouseup={() => (isCollapsed = !isCollapsed)}
+      on:touchend={() => (isCollapsed = !isCollapsed)}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
