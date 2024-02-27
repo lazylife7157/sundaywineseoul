a024601086353be413f3c045325c9ca69fe4c8e2
lazylife7157 <lazylife7157@gmail.com>
2024-02-27 23:22:25 +0900

    Update kakaotalk channel

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -35,7 +35,7 @@
       </svg>
       주문하기
     </a>
-    <a href="https://pf.kakao.com/_UCbHxj/chat" target="_blank">
+    <a href="https://pf.kakao.com/_jlxlJG/chat" target="_blank">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <g>
           <path fill="none" d="M0 0h24v24H0z" />
